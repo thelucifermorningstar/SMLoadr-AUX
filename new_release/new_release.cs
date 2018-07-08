c1aa265a12fce6ac03c8da9ee7a33b97b6abdc17
thelucifermorningstar <37758237+thelucifermorningstar@users.noreply.github.com>
2018-07-08 15:26:00 +0200

    Add files via upload

--- new_release/new_release.cs
+++ new_release/new_release.cs
@@ -14,6 +14,7 @@ namespace SMLoadrAUX.new_release
         public void check()
         {
             string url = "https://github.com/thelucifermorningstar/SMLoadr-AUX/releases/download/1.0.6/SMA_1.0.6_x64.zip";
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
             WebRequest request = WebRequest.Create(url);
 
             try
@@ -23,7 +24,7 @@ namespace SMLoadrAUX.new_release
                 if (response.StatusDescription == "OK")
                 {
 
-                    DialogResult dialog = MessageBox.Show("A new version is avaliable! (1.0.6) Go to Download?", "Update", MessageBoxButtons.YesNo);
+                    DialogResult dialog = MessageBox.Show("A new version is available! (1.0.6) Go to Download?", "Update", MessageBoxButtons.YesNo);
                     if (dialog == DialogResult.Yes)
                     {
                         Process.Start("https://github.com/thelucifermorningstar/SMLoadr-AUX/releases/");
